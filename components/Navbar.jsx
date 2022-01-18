3b1a0e49daf073369b81dfab3752b7ed981be81a
Francogalfre <galfre.vn@gmail.com>
2022-01-18 20:21:37 -0300

    Sample Users

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -618,7 +618,7 @@ function Navbar({ items }) {
                             className="-my-6 divide-y divide-gray-200"
                           >
                             {cartItems.map((product) => (
-                              <li key={product.id} className="py-6 flex">
+                              <li key={product.name.toLowerCase()} className="py-6 flex">
                                 <div className="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
                                   <img
                                     src={product.image1}
