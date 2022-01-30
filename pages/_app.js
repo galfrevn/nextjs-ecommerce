042b086afe3d7a050d17f3b878d6f18ebdfcb4a4
Francogalfre <galfre.vn@gmail.com>
2022-01-30 15:41:26 -0300

    pls

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,12 @@ import { StoreProvider } from "../utils/Store";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 function MyApp({ Component, pageProps }) {
-  
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
   return (
     <StoreProvider>
       <PayPalScriptProvider deferLoading={true}>
