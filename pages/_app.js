d63d6cd3ecf4a62fc72fd10178f545ac31ff15ac
Francogalfre <galfre.vn@gmail.com>
2022-01-30 15:44:34 -0300

    plss

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,10 @@ import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import { StoreProvider } from "../utils/Store";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
+  useEffect (() => {
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
